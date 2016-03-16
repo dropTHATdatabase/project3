e982075797867e462150c642e9f4e22c8752caee
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-16 17:29:04 -0400

    Return entire user row on add/get user from db

--- db/sql/users/add.sql
+++ db/sql/users/add.sql
@@ -1,3 +1,3 @@
 INSERT INTO users(username, password_digest)
 VALUES (${username}, ${password_digest})
-RETURNING user_id, username;
+RETURNING *;
--- db/sql/users/get.sql
+++ db/sql/users/get.sql
@@ -1,4 +1,2 @@
-SELECT user_id, username, hunts_entered, hunts_completed, hunts_won
-FROM users
-WHERE username = ${username} AND
-password_digest = ${password_digest};
+SELECT * FROM users
+WHERE username = ${username}
