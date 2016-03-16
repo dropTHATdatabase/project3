ba43bd90dd426206ba582134fa05a733e0687f28
Razaik Boparai <razaikb@gmail.com>
2016-03-16 16:22:49 -0400

    creates authorization components

---
+++ assets/authComponents/login.js
@@ -0,0 +1,49 @@
+const React = require('react');
+const auth = require('../auth');
+const Login = React.createClass({
+
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
+  getInitialState() {
+    return {
+      error: false
+    }
+  },
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    const name = this.refs.email.value
+    const pass = this.refs.pass.value
+
+    auth.login(email, pass, (loggedIn) => {
+      if (!loggedIn)
+        return this.setState({ error: true })
+
+      const { location } = this.props
+
+      if (location.state && location.state.nextPathname) {
+        this.context.router.replace(location.state.nextPathname)
+      } else {
+        this.context.router.replace('/')
+      }
+    })
+  },
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label><input ref="name" placeholder="name"/></label>
+        <label><input ref="pass" placeholder="password" /></label><br />
+        <button type="submit">login</button>
+        {this.state.error && (
+          <p>Bad login information</p>
+        )}
+      </form>
+    )
+  }
+})
+
+module.exports = Login;
