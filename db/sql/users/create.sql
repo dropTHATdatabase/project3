23e8520465efe97e299edef987ff03f4d831e156
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-16 16:36:10 -0400

    Adds pasword_digest field to users table

--- db/sql/users/create.sql
+++ db/sql/users/create.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
   user_id SERIAL PRIMARY KEY UNIQUE,
   username VARCHAR(255) UNIQUE,
+  password_digest VARCHAR(255) NOT NULL,
   hunts_entered INT NOT NULL DEFAULT 0,
   hunts_completed INT NOT NULL DEFAULT 0,
   hunts_won INT NOT NULL DEFAULT 0
