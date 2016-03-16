0ba9595979402a573917b99f550979d3c222e624
Razaik Boparai <razaikb@gmail.com>
2016-03-16 16:00:50 -0400

    sets structure for app.js

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { browserHistory, Router, Route, Link } from 'react-router'
+import auth from './auth'
+
+const Nav = require('./authComponents/nav.js');
+const SignUp = require('./authComponents/signup.js');
+const CreateHunt = require('./authComponents/createhunt.js');
+const Login = require('./authComponents/login.js');
+const Logout = require('./authComponents/logout.js');
+const auth = require('./auth');
