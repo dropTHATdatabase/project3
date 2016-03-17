2a454a05eba09593422ee2517ed4c80fdf838f89
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-16 21:35:22 -0400

    Return mock data for hunts routes

---
+++ db/hunts.js
@@ -0,0 +1,80 @@
+'use strict';
+
+var mockHunt = {
+  hunt_id: 1,
+  isOwner: true,
+  wager: "Loser buys a beer",
+  deadline: "2016-12-17 07:37:16-08",
+  participants: [
+    {
+      participant_id: 2,
+      username: "ColinRobot",
+      progress: 2
+    },
+    {
+      participant_id: 3,
+      username: "PPPetrov",
+      progress: 4
+    }
+  ],
+  clues: [
+    {
+      clue_id: 1,
+      clue_number: 1,
+      description: 'Where General Assembly Began',
+      lat: 25.0876534,
+      lng: -80.234214
+    },
+    {
+      clue_id: 2,
+      clue_number: 2,
+      description: 'Where General Assembly Began',
+      lat: 25.1276534,
+      lng: -80.344214
+    }
+  ]
+}
+
+
+function add(req, res, next){
+  res.data = mockHunt;
+  next();
+}
+
+function list(req, res, next){
+  res.data = [
+    {
+      hunt_id: 1,
+      owner_id: 1,
+      wager: "Loser buys a beer",
+      winner: null,
+      deadline: "2016-12-17 07:37:16-08"
+    },
+    {
+      hunt_id: 2,
+      owner_id: 1,
+      wager: "Loser buys a beer",
+      winner: null,
+      deadline: "2016-05-17 07:37:16-08"
+    }
+  ];
+
+  next();
+}
+
+function get(req, res, next){
+  res.data = mockHunt;
+
+  next();
+}
+
+function update(req, res, next){
+  res.data = mockHunt;
+
+  next();
+}
+
+function remove(req, res, next){
+
+  next();
+}
