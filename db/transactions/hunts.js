d91bc03a80071e2d087b73e41118389264be96bc
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-17 14:25:29 -0400

    Makes transaction to add participants to hunt

--- db/transactions/hunts.js
+++ db/transactions/hunts.js
@@ -6,6 +6,7 @@ const db = require(path.join(__dirname, '../index'));
 // Insert clues into database and returns a promise
 // Takes an array of clue objects e.g. [{hunt_id:1, clue_number:1,
 // description:"something", lat:25.2334 , lon:-80.342323}, ...]
+// returns array of clue objects
 function insertClues(clues){
   // db.tx executes a transaction
   // i.e. Do not change db until all queries are done
@@ -13,8 +14,9 @@ function insertClues(clues){
     // Create a set of queries to execute and push
     // to an array
     var queries = [];
+    var q;
     for(var i = 0; i < clues.length; i++){
-      var q = this.clues.add(clues[i]); // Add clue i to db
+      q = this.clues.add(clues[i]); // Add clue i to db
       queries.push(q);
     }
     // Runs all querues asynchronously
@@ -22,6 +24,26 @@ function insertClues(clues){
   });
 }
 
+// Inserts participants into the database and
+// returns a promise with an array of perticipan objects
+// expects a hunt_id and array of participant_ids
+function insertParticipants(hunt_id, participant_ids){
+    return db.tx(function(t){
+      var queries = [];
+      var q;
+      for(var i = 0; i < participant_ids.length; i++){
+        q = this.participants.add({
+          hunt_id: hunt_id,
+          participant_id: participant_ids[i]});
+
+        queries.push(q);
+      }
+
+      return this.batch(queries);
+    });
+}
+
 module.exports = {
-  insertClues: insertClues
+  insertClues: insertClues,
+  insertParticipants: insertParticipants
 };
