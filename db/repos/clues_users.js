ee1233ab5125b06c2e061ca65afa6032e1f3164b
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-17 09:30:34 -0400

    Extends pg with tables from database
    * Extends repo based on vitaly-t's demo
      vitaly-t/pg-promise-demo

---
+++ db/repos/clues_users.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const path = require('path');
+const sql = require(path.join(__dirname, '../sql'));
+
+module.exports = function(db) {
+  return {
+    create: function(){
+      return db.none(sql.clues_users.create);
+    }
+  };
+};
