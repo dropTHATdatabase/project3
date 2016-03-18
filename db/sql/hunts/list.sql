4e2ce527802965523fa94ed6e25787aa7053a886
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-18 09:42:41 -0400

    Show winner name in response

--- db/sql/hunts/list.sql
+++ db/sql/hunts/list.sql
@@ -1,13 +1,13 @@
 SELECT h.hunt_id AS hunt_id,
 h.owner_id AS owner_id,
 h.wager AS wager,
-h.deadline AS deadline
---users.user_id AS winner
+h.deadline AS deadline,
+users.user_id AS winner
 FROM hunts AS h
 LEFT JOIN participants AS p
 ON p.hunt_id = h.hunt_id
---LEFT JOIN users
---ON p.participant_id = users.user_id
---AND users.user_id = h.winner_id
+LEFT JOIN users
+ON p.participant_id = users.user_id
+AND users.user_id = h.winner_id
 WHERE h.owner_id = $1 OR
 p.participant_id = $1;
