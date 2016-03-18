f5107359585b7f28a77771cb7aff30e86adfa060
penguyen <peternguyen008@gmail.com>
2016-03-18 09:57:23 -0400

    cleans up code and extra comments

--- assets/authComponents/logout.js
+++ assets/authComponents/logout.js
@@ -1,10 +1,5 @@
 const React = require('react');
 const auth = require('../auth');
-// const App = require('../app.js');
-// const Signup = require('./signup.js');
-// import { browserHistory, Router, Route } from 'react-router'
-
-// const Redirect = Router.Redirect;
 
 const Logout = React.createClass({
   componentDidMount() {
@@ -20,20 +15,5 @@ const Logout = React.createClass({
     )
   }
 })
-
-
-// const Error = React.createClass({     // 404 Error Page
-//   render(){
-//     return(<h1>404 Error - You f*cked up somewhere</h1>)    
-//   }
-// });
-
-// render((
-//   <Router history={browserHistory}>
-//     <Route path="/" component={App} />
-//     <Route path="*" component={Error} />
-//     <Redirect from="logout" to="/" />
-//   </Router>
-// ), document.getElementById('container'))
 
 module.exports = Logout;
