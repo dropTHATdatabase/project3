70227023fc304c8fea4c604c820c10e63f70b344
Razaik Boparai <razaikb@gmail.com>
2016-03-21 14:50:53 -0400

    latest working code

--- assets/components/gameview.js
+++ assets/components/gameview.js
@@ -2,12 +2,11 @@
 import React from 'react'
 import { browserHistory, Router, Route, Link } from 'react-router'
 import auth from '../auth'
-
 const $   = require('jquery');
+const Progress = require('react-progressbar');
 const moment = require('moment');
 const moment_countdown = require('moment-countdown');
 const Createhunt = require('./createhunt.js');
-
 const Map = React.createClass({
   componentDidMount : function() {
    loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyB2U33goCrZ0Hilh_cdksT1_F8jBgUTl4w&libraries=places&callback=plotlocation');
@@ -33,7 +32,6 @@ const Map = React.createClass({
     )
   }
 });
-
 const Gameview = React.createClass({
   contextTypes: {
     user: React.PropTypes.object,
@@ -50,56 +48,10 @@ const Gameview = React.createClass({
     }
   },
   componentWillMount() {
+    localStorage.currentHuntId = parseInt(this.context.currentHuntId)
     // gets list of hunts from user token
-    $.ajax({
-      url: "/api/v1/hunts/"+this.context.currentHuntId,
-      type: "get",
-      beforeSend: function( xhr ) {
-        xhr.setRequestHeader("Authorization", "Bearer " + auth.getToken());
-      }
-    }).done((data)=>{
-      localStorage.currentHuntId = parseInt(this.context.currentHuntId)
-      this.state.game = data.data
-      // setting the state of the game
-      this.setState({ game: this.state.game })
-
-      // grabbing the clues returned from the database
-      // need clue number, lat and lng
-      var clues = this.state.game.clues;
-      var cluesdb =[];
-
-      clues.forEach((el) => {
-        var clueobj ={
-          'clue_number': el.clue_number,
-          'lat': el.lat,
-          'lng': el.lng
-        }
-        cluesdb.push(clueobj)
-      })
-
-      console.log('clues from the database', cluesdb);
-      var cluesstring = JSON.stringify(cluesdb);
-
-      var $hiddenDiv = $('#hidden');
-
-      // remove cluesdb if it already exists
-       $('#cluesdb').remove();
-      // put the clues data from the database into hiddendiv to pass it to google maps script
-      $hiddenDiv.append($('<input id="cluesdb" type="hidden" value='+cluesstring+'>'));
-
-    }).fail((error)=>{
-      console.log('Gameview GET Error: ', error)
-    })
-  },
-  renderClue(clue) {
-    return(<Clue key={clue.clue_id} details={clue} />)
-  },
-  renderParticipant(participant) {
-    return(<Participant key={participant.participant_id} details={participant} />)
-  },
-  handleCheckIn(event) {
-    event.preventDefault();
     navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude, position.coords.longitude)
       var latitude = position.coords.latitude;
       var longitude = position.coords.longitude;
       $.ajax({
@@ -109,98 +61,92 @@ const Gameview = React.createClass({
           xhr.setRequestHeader("Authorization", "Bearer " + auth.getToken());
         }
       }).done((data)=>{
-        console.log('booya: ', data)
+      console.log('data returned ', data)
+      this.state.game = data.data
+      // setting the state of the game
+      this.setState({ game: this.state.game })
+      // grabbing the clues returned from the database
+      // need clue number, lat and lng
+      var clues = this.state.game.clues;
+      var cluesdb =[];
+      clues.forEach((el) => {
+        var clueobj ={
+          'clue_number': el.clue_number,
+          'lat': el.lat,
+          'lng': el.lng
+        }
+        cluesdb.push(clueobj)
       })
+      // console.log('clues from the database', cluesdb);
+      var cluesstring = JSON.stringify(cluesdb);
+      // console.log('json string',cluesstring);
+      var $hiddenDiv = $('#hidden');
+      // console.log('hidden div ', $hiddenDiv);
+      // remove cluesdb if it alreadt
+       $('#cluesdb').remove();
+      // put the clues data from the database into hiddendiv to pass it to google maps script
+      $hiddenDiv.append($('<input id="cluesdb" type="hidden" value='+cluesstring+'>'));
+    }).fail((error)=>{
+      console.log('Gameview GET Error: ', error)
     })
-    console.log('check in button clicked for clue: ', this.state.game.clues)
-  //
-  // localStorage.currentHuntId = parseInt(this.context.currentHuntId)
-  //   // gets list of hunts from user token
-  //   navigator.geolocation.getCurrentPosition((position) => {
-  //     console.log('line120',position.coords.latitude, position.coords.longitude)
-  //     var latitude = position.coords.latitude;
-  //     var longitude = position.coords.longitude;
-  //     $.ajax({
-  //       url: "/api/v1/hunts/"+this.context.currentHuntId+"?lat="+latitude+"&lng="+longitude,
-  //       type: "get",
-  //       beforeSend: function( xhr ) {
-  //         xhr.setRequestHeader("Authorization", "Bearer " + auth.getToken());
-  //       }
-  //     }).done((data)=>{
-  //     console.log('data returned ', data)
-  //     console.log('line131');
-  //     this.state.game = data.data
-  //     // setting the state of the game
-  //     // this.setState({ game: this.state.game })
-  //     console.log('line134',this.state.game )
-  //     // grabbing the clues returned from the database
-  //     // need clue number, lat and lng
-  //     $('#cluesdb').remove();
-  //     var clues = this.state.game.clues;
-  //     console.log('line137', clues);
-  //     var cluesdb =[];
-  //     clues.forEach((el) => {
-  //       var clueobj ={
-  //         'clue_number': el.clue_number,
-  //         'lat': el.lat,
-  //         'lng': el.lng
-  //       }
-  //       cluesdb.push(clueobj)
-  //     })
-  //     // console.log('clues from the database', cluesdb);
-  //     var cluesstring = JSON.stringify(cluesdb);
-  //     // console.log('json string',cluesstring);
-  //     var $hiddenDiv = $('#hidden');
-  //     // console.log('hidden div ', $hiddenDiv);
-  //     // remove cluesdb if it alreadt
-  //
-  //     // put the clues data from the database into hiddendiv to pass it to google maps script
-  //     $hiddenDiv.append($('<input id="cluesdb" type="hidden" value='+cluesstring+'>'));
-  //
-  //     this.forceUpdate();
-  //   }).fail((error)=>{
-  //     console.log('Gameview GET Error: ', error)
-  //   })
-  // })
-
+  })
+  },
+  renderClue(clue) {
+    return(<Clue key={clue.clue_id} details={clue} />)
+  },
+  renderParticipant(participant) {
+    return(<Participant key={participant.participant_id} details={participant} />)
+  },
+  handleCheckIn(event) {
+    event.preventDefault();
+    if(this.state.game.clues.length) {
+      console.log('clue length: ', this.state.game.clues.length)
+      console.log('check in button clicked for clue: ', this.state.game.clues[this.state.game.clues.length-1])    // is this the correct last clue?
+    }
+    $.ajax({
+      url: "/api/v1/hunts/"+ this.context.currentHuntId +"/clues/"+this.state.game.clues[this.state.game.clues.length-1].clue_id,
+      type: "PUT",
+      beforeSend: function( xhr ) {
+        xhr.setRequestHeader("Authorization", "Bearer " + auth.getToken());
+      }
+    }).done((data)=>{
+      console.log('next clue?: ', data)
+    })
   },
   render() {
     var clues = this.state.game.clues;
     var participants = this.state.game.participants;
-
+    // console.log('deadline: ',this.state.game.deadline)
+    console.log('hunt game: ', this.state.game)
+    // console.log('participants: ', this.state.game.participants)
     return (
       <div>
-        <div className="center-align">
-          <h2 id="wager">{this.state.game.wager}</h2>
-          <h3 id="time">{moment(this.state.game.deadline).countdown().toString()}</h3>
-        </div>
-
+        <h2>{this.state.game.wager}</h2>
+        <h3 id="time">{moment(this.state.game.deadline).countdown().toString()}</h3>
 
         <div className="row">
           {/* List of all User hunts + Edit|View|Delete options per hunt */}
-          <div className="card-panel z-depth-5 gameview clues center-align">
-            <h5 id="clues">Clues:</h5>
-            <ul className="text collection">
+          <div className="gameview clues">
+            <h5>Clues:</h5>
+            <ul>
               {/* List all clues here */}
               { clues ? clues.map((el)=> this.renderClue(el)) : console.log('no clues available') }
             </ul>
           </div>
-
           <div className="map">
             <Map />
           </div>
-
           {/* User Hunt Record + Create Hunt btn */}
-          <div className="card-panel z-depth-5 gameview status center-align">
-            <h5 id="clues">Player Status:</h5>
-            <ul className="text collection">
+          <div className="gameview status">
+            <h5>Player Status:</h5>
+            <div>
               {/* List each player status here */}
               { participants ? participants.map((el)=> this.renderParticipant(el)) : console.log('no participants available') }
-            </ul>
+            </div>
             <div>
-              { this.state.game.isOwner
-                  ? console.log('creator view')
-                  : <button className="waves-effect waves-light btn" onClick={this.handleCheckIn}>Check In</button> }
+              { this.state.game.showNextClue
+                  ? <button onClick={this.handleCheckIn}>Check In</button>
+                  : console.log('showNextClue is false') }
             </div>
           </div>
         </div>
@@ -209,22 +155,21 @@ const Gameview = React.createClass({
     )
   }
 });
-
 const Clue = React.createClass({
   render() {
     return ( <li>{this.props.details.description}</li> )
   }
 });
-
 const Participant = React.createClass({
+    // $( "#progressbar" ).progressbar({
+    //   value: this.props.details.progress
+    // });
   render() {
     return (
       <div>
-        {this.props.details.username}: {this.props.details.progress}
+        {this.props.details.username}: <div><Progress completed={this.props.details.progress*100} /></div>
       </div>
     )
   }
 });
-
-
 module.exports = Gameview;
